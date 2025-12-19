33f4df74f621628df9cefbb25fe2c94ce8da99b5
Charlles-af <charllesaraujoreal@gmail.com>
2025-12-19 18:07:15 -0300

    Atualização

---
+++ PHP/basico/html.php
@@ -0,0 +1,13 @@
+<div class="titulo">Integração HTML</div>
+
+<h1>
+    <?php
+    echo 'Olá'; 
+    echo '<small> Mundo! </small>';
+    ?>
+</h1>
+
+<?= "<div>Outra Forma de me 'expressar'!</div>" ?>
+
+<br>
+<div><button><?= "Legal" ?></button></div>
--- PHP/basico/ola.php
+++ PHP/basico/ola.php
@@ -1,7 +1,14 @@
 <h1>Ola PHP</h1>
 
+<h2> Exemplo A</h2>
 <?php 
 echo 'Olá <br>';
 echo 'Mundo!';
 ?>
 
+<h2> Exemplo B</h2>
+<?= "outra forma de me 'expressar!'" ?>
+<?= 2 * 3 + 1 ?>
+
+<?php 
+phpinfo(); 
